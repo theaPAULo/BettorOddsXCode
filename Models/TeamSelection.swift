3c92fa77da144ac4f9179ee6442c24be3f85932f
Paul <psoni511@gmail.com>
2025-01-27 17:00:45 -0600

    UI enhancements pt 2

---
+++ Models/TeamSelection.swift
@@ -0,0 +1,13 @@
+//
+//  TeamSelection.swift
+//  BettorOdds
+//
+//  Created by Paul Soni on 1/27/25.
+//  Version: 1.0.0
+
+import Foundation
+
+enum TeamSelection {
+    case home
+    case away
+}
